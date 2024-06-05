b0c8d0a273fa43800f5f489cf0b4a06c553e094d
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-06-05 12:38:02 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,29 @@
+let score="hitesh"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN
+null => 0
+undefined => NaN
+true => 1
+false => 0
+
+*/
+let islogin=1
+let booleanisloggedin=Boolean(islogin)
+console.log(booleanisloggedin)
+
+/*
+1 => true
+0 => false
+"" => false
+"Nishant" => true
+*/
+let numberbe=44
+let string_number=String(numberbe)
+console.log(typeof string_number)
+console.log(string_number)
