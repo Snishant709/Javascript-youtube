4dfc880ac063aea8c1cac68d92587d4970d6c773
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-06-06 07:10:48 +0000

    comparison

---
+++ 01_basics/04_Comparsion.js
@@ -0,0 +1,11 @@
+console.log(2>1)
+console.log("2">1)
+console.log("0">1)
+
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//strict check
+console.log("2"===2)//it will give false as it checks the datatype and dont convert the datatype through which it is compared
