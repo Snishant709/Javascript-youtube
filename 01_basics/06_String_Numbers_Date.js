c43d8858a30af9de538d3a22ee58ab251413303f
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-06-08 09:14:48 +0000

    Date

--- 01_basics/06_String_Numbers_Date.js
+++ 01_basics/06_String_Numbers_Date.js
@@ -1,3 +1,4 @@
+/*
 //************String********************
 const name="Nishant"
 console.log(name)
@@ -16,8 +17,40 @@ for(i=0;i<surname.length;i++)
 
    // *********************Numbers**********************
 
-   const num=2;
+   const num=22.34;
    console.log(num)
    let n=new Number(2);
-   console.log(n.toString())
+   console.log(num.toString().length)
    console.log(typeof n)
+   console.log(num.toPrecision(2))
+   const n1=new Number(100000)
+   console.log(n1.toLocaleString('en-IN'))
+   //***************Math************************
+   console.log(Math.abs(-4))
+   console.log(Math.round(5263682.322))
+   console.log(Math.floor(566.444))
+   console.log(Math.ceil(2324.2))
+   console.log(Math.min(3,566,11,1))
+   console.log(Math.random())
+   console.log(Math.round(Math.random()*10)+1)
+   const min=5
+   const max=10
+   console.log(Math.round(Math.random()*(max-min+1))+min)
+   */
+   //******************* Date**********************
+   let mydate=new Date()
+   console.log(mydate.getDay())
+   console.log(mydate.toLocaleDateString())
+   let yourchoicedate=new Date("08-06-2024")
+   console.log(yourchoicedate.toDateString())
+   let anotherdate=new Date(2024,5,8)
+   console.log(anotherdate.toString())
+   console.log(anotherdate.toDateString())
+   console.log(anotherdate.toLocaleDateString())
+
+   let mytimestamp= Date.now()
+   console.log(mytimestamp)
+   let newdate=new Date()
+   console.log(newdate.toLocaleString('default',{weekday:"long"}))
+
+
