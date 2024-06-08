af56b12b903ae652ca202d4ad7446bd1556fbd72
Snishant709 <117982133+Snishant709@users.noreply.github.com>
2024-06-08 06:51:29 +0000

    Practice

---
+++ 01_basics/06_String_Numbers_Date.js
@@ -0,0 +1,23 @@
+//************String********************
+const name="Nishant"
+console.log(name)
+console.log(name[0])
+const surname=new String('Shar-ma')
+console.log(surname)
+let i=0;
+for(i=0;i<surname.length;i++)
+    {
+        console.log(surname[i])
+    }
+    console.log(typeof name)
+    console.log(typeof surname)
+    console.log(surname.charAt(0))
+    console.log(surname.split('-'))
+
+   // *********************Numbers**********************
+
+   const num=2;
+   console.log(num)
+   let n=new Number(2);
+   console.log(n.toString())
+   console.log(typeof n)
